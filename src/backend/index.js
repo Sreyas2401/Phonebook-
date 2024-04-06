5e3931eba1277b64226b35934a981c8c0e001a39
Sreyas2401 <srajasekharu@umass.edu>
2024-04-05 21:04:10 -0400

    created backend with express and set up backend npm scripts

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,33 +1,87 @@
 const express = require('express');
-const http = require('http');
+const { request } = require('http');
+const app = express();
 
+app.use(express.json());
 
-const people = {
-  "persons": [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": "1"
+let people = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
     },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": "2"
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
     },
-    {
+    { 
       "id": 3,
-      "name": "Sreyas",
-      "number": "12-34-567890"
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
     }
-  ]
-}
+]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.end(JSON.stringify(people));
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.get('/info', (request, response) => {
+  const currentTime = new Date().toLocaleString()
+  response.send(`
+  <p>Phonebook has info for ${people.length} people</p>
+  <p>${currentTime}</p>
+  `)
+});
 
+app.get('/api/people', (request, response) => {
+  response.json(people)
+});
+
+app.get('/api/people/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = people.find((p) => 
+    p.id === id
+  )
+  person ? response.json(person) : response.status(404).end()
+})
+
+app.delete('/api/people/:id', (request, response) => {
+  const id = Number(request.params.id);
+  people = people.filter(person => person.id !== id);
+  response.status(204).end();
+});
+
+
+app.post('/api/people', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'Name or number missing' 
+    });
+  }
+
+  const maxId = people.length > 0 ? Math.max(...people.map(person => person.id)) + 1 : 1;
+
+  const person = {
+    id: maxId,
+    name: body.name,
+    number: body.number
+  };
+
+  people = people.concat(person);
+
+  response.status(201).json(person);
+});
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
